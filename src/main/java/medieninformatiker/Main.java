e8461a7e798f2196a028a40422632db3db0589f6
SkazuHD <81975854+SkazuHD@users.noreply.github.com>
2022-08-08 21:37:45 +0200

    Asks to Restore when started

--- src/main/java/medieninformatiker/Main.java
+++ src/main/java/medieninformatiker/Main.java
@@ -1,15 +1,25 @@
 package medieninformatiker;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
+        Scanner input = new Scanner(System.in);
         Book note = new Book();
         note.clearScreen();
-        note.loadEntry();
+
+
+
+
         System.out.println("Welcome im your personal Notebook");
+        System.out.println("Restore Notebook from File? (Y/N)");
+        if(input.nextLine().equals("Y")){
+            note.clearScreen();
+            note.loadEntry();
+
+        }
         while (true){
 
             note.showOptions();
